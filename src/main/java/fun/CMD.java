d7d23338869f9247d8728bf3bff2d23d3fa5e5bd
thl23 <thl23@aber.ac.uk>
2025-03-16 00:22:36 +0000

    Convert project to Maven

--- src/main/java/fun/CMD.java
+++ src/main/java/fun/CMD.java
@@ -1,10 +1,6 @@
 import javafx.application.Application;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-import javafx.scene.layout.BackgroundFill;
 
 public class CMD extends Application {
     @Override
--- src/main/java/fun/Fibonacci.java
+++ src/main/java/fun/Fibonacci.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
